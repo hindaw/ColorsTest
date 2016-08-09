7985087ce5b43b98a2e971a85b3282559970e603
hindaw <hinda.weissbecker@gmail.com>
2016-08-09 18:40:01 +0300

    first commit

---
+++ Colors/Controllers/DB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Colors.Controllers
+{
+    public static class DB
+    {
+
+        public static List<Colors> Colors = new List<Colors>()
+        {
+            new Colors(){Id = 1, Name = "Red"},
+            new Colors(){Id = 2, Name = "Orange"},
+            new Colors(){Id = 3, Name = "Yellow"},
+            new Colors(){Id = 4, Name = "Green"},
+            new Colors(){Id = 5, Name = "Blue"},
+            new Colors(){Id = 6, Name = "Indigo"},
+            new Colors(){Id = 7, Name = "Violet"},
+        };
+
+
+        public static List<Votes> VotesList = new List<Votes>()
+        {
+            new Votes(){City = "Anchorage", Color =5 ,VotesNum=10000},
+            new Votes(){City = "Anchorage", Color =3 ,VotesNum=15000},
+            new Votes(){City = "Brooklyn", Color = 1 ,VotesNum=100000},
+            new Votes(){City = "Brooklyn", Color = 5 ,VotesNum=250000},
+            new Votes(){City = "Detroit", Color =1 ,VotesNum=160000},
+            new Votes(){City = "Selma", Color = 3 ,VotesNum=15000},
+            new Votes(){City = "Selma", Color = 7,VotesNum=5000},
+        };
+
+
+
+    }
+
+    public class Colors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class Votes
+    {
+        public string City { get; set; }
+        public int Color { get; set; }
+        public double VotesNum { get; set; }
+    }
+}
---
+++ Colors/Controllers/DataController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Colors.Controllers
+{
+    public class DataController : ApiController
+    {
+        /// <summary>
+        /// This function returns a list of colors
+        /// </summary>
+        /// <returns>List</returns>
+        public List<Colors> Get()
+        {
+            return DB.Colors.ToList();
+        }
+
+        /// <summary>
+        /// This function totals votes by color code
+        /// </summary>
+        /// <param name="colorId"></param>
+        /// <returns>int</returns>
+
+        [HttpGet]
+        public double GetVotes(int colorId)
+        {
+            return DB.VotesList.Where(x => x.Color == colorId).Sum(c => c.VotesNum);
+
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+
+        
+    }
+}
